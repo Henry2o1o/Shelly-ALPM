6fd99e8bb30da3035f5c9590bd316475ca941966
VinnyQF <viniqf@gmail.com>
2026-05-12 12:52:55 -0300

    Creating commandsettings

--- Shelly-CLI/Commands/Flatpak/FlatpakRepairCommand.cs
+++ Shelly-CLI/Commands/Flatpak/FlatpakRepairCommand.cs
@@ -6,7 +6,11 @@ using Spectre.Console.Cli;
 
 namespace Shelly_CLI.Commands.Flatpak;
 
-public class FlatpakRepairCommand
+public class FlatpakRepairCommand : CommandSettings
 {
-    
+    [CommandOption("-u|--user")]
+    public bool User { get; set; }
+    [CommandOption("--dry-run")]
+    public bool DryRun { get; set; }
 }
+
