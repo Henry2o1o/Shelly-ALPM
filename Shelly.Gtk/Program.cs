0ab87a13d31f95d29d7516255628cb0c4ced0224
Anton Ždanov <anton@azdanov.dev>
2026-06-02 12:45:11 +0300

    Remove unnecessary `TrimExcess` and manual garbage collection calls in `PackageInstall`
    Should be careful touching GC, since it can cause stop the world event: https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/performance#troubleshoot-performance-issues

--- Shelly.Gtk/Program.cs
+++ Shelly.Gtk/Program.cs
@@ -285,8 +285,6 @@ sealed class Program
 
                 foreach (var w in windows)
                     w.Dispose();
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
             }
 
             void LoadPackagesPage()
