16845c1f6b205e20bd84bb39e40f2a50a6a373ac
Anton Ždanov <anton@azdanov.dev>
2026-06-16 15:11:33 +0300

    Fix `NO_COLOR` env var case

--- Shelly.Cli/Interactions/AnsiUtilities.cs
+++ Shelly.Cli/Interactions/AnsiUtilities.cs
@@ -5,12 +5,13 @@ namespace Shelly.Cli.Interactions;
 
 internal static class AnsiUtilities
 {
-    internal static bool SupportsAnsi => !Console.IsOutputRedirected && !Console.IsInputRedirected &&
-                                         Environment.GetEnvironmentVariable("TERM") != "dumb" &&
-                                         Environment.GetEnvironmentVariable("NO_Color") is null;
-    
+    internal static bool SupportsAnsi =>
+        !Console.IsOutputRedirected && !Console.IsInputRedirected &&
+        Environment.GetEnvironmentVariable("TERM") != "dumb" &&
+        Environment.GetEnvironmentVariable("NO_COLOR") is null;
+
     internal static string Colorize(string text, Color color)
     {
-         return SupportsAnsi ? text.Pastel(color) : text;
+        return SupportsAnsi ? text.Pastel(color) : text;
     }
 }
