3ef9e5d5837344debc197913b1795e98ac3df672
Anton Ždanov <anton@azdanov.dev>
2026-06-16 15:40:15 +0300

    Handle process disposal in `EnsureRootExecution`

--- Shelly.Cli/RootElevator.cs
+++ Shelly.Cli/RootElevator.cs
@@ -6,27 +6,19 @@ public static class RootElevator
 {
     public static void EnsureRootExectuion()
     {
-        if (Environment.UserName.Equals("root", StringComparison.OrdinalIgnoreCase))
-        {
-            return;
-        }
+        if (Environment.UserName.Equals("root", StringComparison.OrdinalIgnoreCase)) return;
 
         var args = Environment.GetCommandLineArgs();
         var exe = Environment.ProcessPath ?? args[0];
-        var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "sudo",
-                ArgumentList = { exe },
-                UseShellExecute = false,
 
-            }
-        };
-        foreach (var arg in args.Skip(1))
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo
         {
-            process.StartInfo.ArgumentList.Add(arg);
-        }
+            FileName = "sudo",
+            ArgumentList = { exe },
+            UseShellExecute = false
+        };
+        foreach (var arg in args.Skip(1)) process.StartInfo.ArgumentList.Add(arg);
 
         process.Start();
         process.WaitForExit();
