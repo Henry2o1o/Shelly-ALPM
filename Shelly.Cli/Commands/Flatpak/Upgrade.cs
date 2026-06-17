3fc282228279226efc83382e57247a3bba4688e5
Caroline Snyder <carolinemariesnyder@gmail.com>
2026-06-17 16:21:05 -0400

    updating flatpak upgrade command.

--- Shelly.Cli/Commands/Flatpak/Upgrade.cs
+++ Shelly.Cli/Commands/Flatpak/Upgrade.cs
@@ -30,14 +30,20 @@ public class Upgrade : GlobalSettingsCommand
         }
 
         console.WriteLine(Colorize("Updating all flatpak apps...", ConsoleColor.Yellow));
-        var result = FlatpakManager.UpdateAllFlatpak();
+        var result = FlatpakManager.UpdateAllUserFlatpak();
+        console.WriteLine(Colorize(result, ConsoleColor.Yellow));
+
+        result = FlatpakManager.UpdateAllSystemFlatpak();
         console.WriteLine(Colorize(result, ConsoleColor.Yellow));
     }
 
     public override ValueTask ExecuteUiMode()
     {
         UiFrames.Info("Updating all flatpak apps...", Shelly.Utilities.Eventing.AlpmEvents.TransactionStart);
-        var result = FlatpakManager.UpdateAllFlatpak();
+        var result = FlatpakManager.UpdateAllUserFlatpak();
+        UiFrames.Info(result);
+
+        result = FlatpakManager.UpdateAllSystemFlatpak();
         UiFrames.Info(result);
         UiFrames.TxFinish(true, "Flatpak upgrade complete.", "Flatpak upgrade failed.");
         return ValueTask.CompletedTask;
