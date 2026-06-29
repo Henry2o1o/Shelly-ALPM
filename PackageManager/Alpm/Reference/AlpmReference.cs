0fc14c2e0a7a1b3b69e7c49322a53487119281a6
Zoey <zoey.erin.bauer@gmail.com>
2026-06-29 18:31:44 -0400

    Add `Directory.Build.props` for shared project settings and clean up related project files.

---
+++ PackageManager/Alpm/Reference/AlpmReference.cs
@@ -0,0 +1,10 @@
+// using System.Reflection;
+// using System.Runtime.CompilerServices;
+//
+// namespace PackageManager.Alpm.Reference;
+//
+// [assembly: InternalsVisibleTo("PackageManager.Tests")]
+// internal static partial class AlpmReference
+// {
+//     
+// }
