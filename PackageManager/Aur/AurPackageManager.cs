ff0623bde902ae43723654015f5e51f4dabca012
NateKing204 <nate.john.king@gmail.com>
2026-07-04 23:34:19 -0500

    AUR Remove now shows the local installs correct version.

--- PackageManager/Aur/AurPackageManager.cs
+++ PackageManager/Aur/AurPackageManager.cs
@@ -138,7 +138,11 @@ public sealed class AurPackageManager(string? configPath = null)
         var aurPackages = response.Results;
         foreach (var pkg in aurPackages)
         {
+            var localMatch = foreignPackages.First(x => x.Name == pkg.Name);
+            
             pkg.Explicit = foreignPackages.First(x => x.Name == pkg.Name).InstallReason == "Explicit";
+            
+            pkg.Version = localMatch.Version;
         }
 
         return aurPackages;
