b82b895287abd3f8b2588e262321255fa63e9a0c
VinnyQF <viniqf@gmail.com>
2026-07-28 09:45:28 -0300

    Adding first bindings for ostree repo

--- Shelly.PackageManager/src/ostree/bindings.zig
+++ Shelly.PackageManager/src/ostree/bindings.zig
@@ -3,5 +3,23 @@ const std = @import("std");
 pub const libostree = struct {
     pub const ostree = @import("ostree");
 
+    pub fn str(ptr: [*c]const u8) ?[:0]const u8 {
+        if (ptr == null) return null;
+        return std.mem.span(ptr);
+    }
 
+    pub const Repo = struct {
+        ptr: *ostree.OstreeRepo,
+
+        pub fn init(ptr: *ostree.OstreeRepo) Repo {
+            return .{
+                .ptr = ptr,
+            };
+        }
+
+        pub fn path(self: Repo) ?*ostree.GFile {
+            return ostree.ostree_repo_get_path(self.ptr);
+        }
+        
+    };
 };
