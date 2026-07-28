17c50127ed4a03ac74f708405cd075849fdf2fc9
VinnyQF <viniqf@gmail.com>
2026-07-28 09:50:52 -0300

    Adding task list for all functions that need to be ported over

--- Shelly.PackageManager/src/ostree/bindings.zig
+++ Shelly.PackageManager/src/ostree/bindings.zig
@@ -1,5 +1,32 @@
 const std = @import("std");
 
+// Methods to port over for Flatpak Repair
+// --
+// Repo
+// --
+// ostree_repo_fsck_object()
+// ostree_repo_delete_object()
+// ostree_repo_load_variant()
+// ostree_repo_load_commit()
+// ostree_repo_list_refs()
+// ostree_repo_set_ref_immediate()
+// ostree_repo_mark_commit_partial_reason()
+// --
+// Commit traversal
+// ostree_repo_commit_traverse_iter_init_dirtree()
+// ostree_repo_commit_traverse_iter_next()
+// ostree_repo_commit_traverse_iter_get_file()
+// ostree_repo_commit_traverse_iter_get_dir()
+// --
+// Helpers
+// --
+// ostree_parse_refspec()
+// ostree_checksum_from_bytes()
+// ostree_checksum_bytes_peek()
+// ostree_object_name_serialize()
+// ostree_object_type_to_string()
+// 
+
 pub const libostree = struct {
     pub const ostree = @import("ostree");
 
