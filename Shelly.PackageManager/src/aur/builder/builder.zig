7c43f0a4fac2abb97b5d977da7ee32f35b06768f
Zoey <zoey.erin.bauer@gmail.com>
2026-08-09 15:25:54 -0400

    updated builder.

--- Shelly.PackageManager/src/aur/builder/builder.zig
+++ Shelly.PackageManager/src/aur/builder/builder.zig
@@ -1,26 +1,52 @@
 const std = @import("std");
 const MakePkgConfiguration = @import("../makepackage.zig").MakePackageConfiguration;
 const PackageBuild = @import("../../pkgbuild/pkgbuild_parser.zig").Pkgbuild;
+const events = @import("../events.zig");
+const op_context = @import("../../shared/operation_context.zig");
 
-pub const Builder = struct {
+/// Builder expects to be passed all items and should not construct
+/// these items as it's context only exists to serve inside manager
+pub const PackageBuilder = struct {
     allocator: std.mem.Allocator,
     package_build: PackageBuild,
+    dispatcher: events.Dispatcher,
+    operation_context: op_context.OperationContext,
+    makepkg_config: MakePkgConfiguration,
 
-    pub fn init(allocator: std.mem.Allocator, package_build: PackageBuild) !*Builder {
-        const self = allocator.create(Builder) catch |err| {
+    pub fn init(
+        allocator: std.mem.Allocator,
+        package_build: PackageBuild,
+        dispatcher: events.Dispatcher,
+        operation_context: op_context.OperationContext,
+        makepackage_configuration: MakePkgConfiguration,
+    ) !*PackageBuilder {
+        const self = allocator.create(PackageBuilder) catch |err| {
             return err;
         };
 
-        self.* = Builder{
+        self.* = PackageBuilder{
             .allocator = allocator,
             .package_build = package_build,
+            .dispatcher = dispatcher,
+            .operation_context = operation_context,
+            .makepkg_config = makepackage_configuration,
         };
 
         return self;
     }
 
-    pub fn deint(self: *Builder) !void {
+    pub fn deint(self: *PackageBuilder) !void {
         self.package_build.deinit(self.allocator);
+        self.dispatcher.deinit();
         self.*;
+    }
+
+    pub fn BuildPackage(self: *PackageBuilder) !void {
+        const buildPath = self.makepkg_config.build_directory;
+        for (self.package_build.execution_steps.?) |step| {
+            const child: std.process.Child = .init{&.{ "/bin/sh", "-c", step.body }};
+            _ = buildPath;
+            _ = child;
+        }
     }
 };
