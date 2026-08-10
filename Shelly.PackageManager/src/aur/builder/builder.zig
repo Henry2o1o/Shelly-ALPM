ce5d5d562d89a8f9c70b282896e0a19569b63b77
Zoey <zoey.erin.bauer@gmail.com>
2026-08-10 01:21:19 -0400

    added build process

--- Shelly.PackageManager/src/aur/builder/builder.zig
+++ Shelly.PackageManager/src/aur/builder/builder.zig
@@ -12,6 +12,7 @@ pub const PackageBuilder = struct {
     dispatcher: events.Dispatcher,
     operation_context: op_context.OperationContext,
     makepkg_config: MakePkgConfiguration,
+    io: std.Io,
 
     pub fn init(
         allocator: std.mem.Allocator,
@@ -19,6 +20,7 @@ pub const PackageBuilder = struct {
         dispatcher: events.Dispatcher,
         operation_context: op_context.OperationContext,
         makepackage_configuration: MakePkgConfiguration,
+        io: std.Io,
     ) !*PackageBuilder {
         const self = allocator.create(PackageBuilder) catch |err| {
             return err;
@@ -30,6 +32,7 @@ pub const PackageBuilder = struct {
             .dispatcher = dispatcher,
             .operation_context = operation_context,
             .makepkg_config = makepackage_configuration,
+            .io = io,
         };
 
         return self;
@@ -42,11 +45,33 @@ pub const PackageBuilder = struct {
     }
 
     pub fn BuildPackage(self: *PackageBuilder) !void {
-        const buildPath = self.makepkg_config.build_directory;
+        var operation = self.operation_context.begin(op_context.OperationDescriptor{ .backend = .aur, .kind = .build, .subject = "Package Build" });
+        defer operation.finish(.cancelled);
         for (self.package_build.execution_steps.?) |step| {
-            const child: std.process.Child = .init{&.{ "/bin/sh", "-c", step.body }};
-            _ = buildPath;
-            _ = child;
+            var child = try std.process.spawn(self.io, .{ .argv = &.{ "/bin/sh", "-c", step.expanded_body }, .stdout = .inherit, .stderr = .inherit, .cwd = self.makepkg_config.build_directory });
+            const term = try child.wait(self.io);
+            switch (term) {
+                .exited => |code| {
+                    if (code != 0) {
+                        self.dispatcher.raiseError(.{
+                            .message = code,
+                        });
+                        operation.finish(.failed);
+                        return;
+                    }
+                    self.dispatcher.raiseInformational(.{
+                        .message = "Finished step",
+                    });
+                },
+                .signal => {
+                    self.dispatcher.raiseError(.{
+                        .message = "Failed step",
+                    });
+                    operation.finish(.failed);
+                    return;
+                },
+            }
         }
+        operation.finish(.success);
     }
 };
