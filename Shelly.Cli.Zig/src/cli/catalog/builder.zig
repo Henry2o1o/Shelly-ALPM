13bf2a02199833e8f16bd5382198f5c25ba4ee42
Zoey <zoey.erin.bauer@gmail.com>
2026-08-16 23:48:22 -0400

    added missing options to variant

--- Shelly.Cli.Zig/src/cli/catalog/builder.zig
+++ Shelly.Cli.Zig/src/cli/catalog/builder.zig
@@ -8,9 +8,11 @@ pub const variants = [_]types.Variant{.{
     .name = "build",
     .default_for_action = true,
     .description = "Builds a PKGBUILD into an installable package",
-    .options = &.{
-        flag("--reviewed", &.{"-r"}, "Marks the package as reviewed"),
-        flag("--sync-deps", &.{"-s"}, "Installs missing dependencies"),
-    },
+    .options = &.{ flag("--reviewed", &.{"-r"}, "Marks the package as reviewed"), flag("--sync-deps", &.{"-s"}, "Installs missing dependencies"), flag(
+        "--check",
+        &.{"-c"},
+        "Performs check on PKGBUILD and installs check depends",
+        flag("--isolated", &.{"-i"}, "Runs isolated root build. (This is currently nonfunctional and is being worked on"),
+    ) },
     .arguments = &.{types.optionalArgument("pkgbuild", "File path of PKGBUILD. If left empty will look in executed directory for file named PKGBUILD.")},
 }};
